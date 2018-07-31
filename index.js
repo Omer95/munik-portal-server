97171729aed53e2914c8014e3c21e888c5c27fd3
Omer Farooq <omer.farooq@tenpearls.com>
2018-07-31 12:22:26 +0500

    user authentication with sessions complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const fileStore = require('session-file-store')(session);
 const delegationRouter = require('./routes/delegationRouter');
+const userRouter = require('./routes/userRouter');
 const mongoose = require('mongoose');
 const Delegations = require('./models/delegations');
 
@@ -15,7 +16,7 @@ const connect = mongoose.connect(url);
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
 app.use(session({
     name: 'session-id',
     secret: '12345-67890-09876-54321',
@@ -23,49 +24,29 @@ app.use(session({
     resave: false,
     store: new fileStore()
 }));
+app.use('/users', userRouter);
 
 function auth(req, res, next) {
     console.log(req.session);
     if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error('You are not authenticated!');
-            // res.setHeader('WWW-Authenticate', 'Basic');
-            // res.status = 401;
-            res.writeHead(401, {"WWW-Authenticate": "Basic"});
-            res.end('Authentication is needed');
-            next(err);
-            return;
-        }
-        const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const user = auth[0];
-        const password = auth[1];
-        if (user === 'admin' && password === 'password') {
-            // res.cookie('user', 'admin', {signed: true});
-            req.session.user = 'admin';
-            next();
-        }
-        else {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            res.status = 401;
-            next(err);
-        }
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
     }
     else {
-        if (req.session.user === 'admin') {
-            console.log('req.session: ', req.session);
+        if (req.session.user === 'authenticated') {
             next();
         }
         else {
-            err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
+            let err = new Error('You are not authenticated!');
+            err.status = 403;
+            return next(err);
         }
     }
 }
 app.use(auth);
 app.use('/delegation', delegationRouter);
+
 
 connect.then((db) => {
     console.log('Connected correctly to server');
